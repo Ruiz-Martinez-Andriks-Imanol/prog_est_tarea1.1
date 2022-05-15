4f79518f033cd9be3842e8253f277fbc3099a117
Ruiz-Martinez-Andriks-Imanol <105221206+Ruiz-Martinez-Andriks-Imanol@users.noreply.github.com>
2022-05-14 21:56:08 -0500

    prog_est_tarea1.1
    Calcula el área de la corona circular

---
+++ Tarea1.1.cpp
@@ -0,0 +1,31 @@
+/* Autor: Andriks Imanol Ruiz Martínez, Realizado: 03/02/2022 
+	Escuela: Universidad del Valle de Mexico Campus Villahermosa
+	Materia: Programación Estructrada
+	Ciclo: 01/2022
+
+	Este es un programa en Lenguaje C de la materia de Programación Estructurada
+	Muestra el uso de:
+
+    	-Variables flotantes
+    	-printf para mostrar mensajes y variables
+    	-scanf
+    	-El uso de include para las librerías
+    	-uso de la librería math.h
+    	-Comentarios para la documentación interna del programa
+*/
+#include<stdio.h>
+#include<math.h>
+int main(){
+	//Declaración de variables
+	float R, r,a;
+	//Entrada de datos
+	printf("Introduce el radio menor de la corona: ");
+	scanf("%f", &r);
+	printf("Introduce el radio mayor de la corona: ");
+	scanf("%f", &R);
+	//Proceso
+	a=M_PI*pow(r,2)-(R,2);
+	//Salida
+	printf("El area de la corona es %f", a);
+	return 0;
+}
